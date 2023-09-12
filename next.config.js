bffbe8728d80ccc2a6a8a346104c0fb34108b795
Dominique Cuevas <dominiquecuevas@Dominiques-MacBook-Pro.local>
2023-09-11 22:15:03 -0700

    replace username with avatar

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {hostname: '**.googleusercontent.com'},
+      {hostname: '**.githubusercontent.com'}
+    ]
+  }
 }
 
 module.exports = nextConfig
